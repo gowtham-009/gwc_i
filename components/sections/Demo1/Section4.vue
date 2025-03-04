e570ed47850c81182c6357aaf853c7b284a00438
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-04 16:47:48 +0530

    Update Section4.vue

--- components/sections/Demo1/Section4.vue
+++ components/sections/Demo1/Section4.vue
@@ -1,5 +1,4 @@
 <template>
-    <!-- FEATURES-6 -->
     <section id="features-6" class="py-100 features-section division">
         <div class="container">
             <!-- SECTION TITLE -->
@@ -7,15 +6,15 @@
                 <div class="col-md-10 col-lg-9">
                     <div class="section-title mb-70">
                         <!-- Title -->
-                        <h2 class="s-50 w-700">Why Partner with Us?</h2>
+                        <h2 class="s-50 w-700">The Complete Solutions</h2>
                         <!-- Text -->
                         <p class="s-21 color--grey">Ligula risus auctor tempus magna feugiat lacinia.</p>
                     </div>
                 </div>
             </div>
             <!-- FEATURES-6 WRAPPER -->
-            <div class="fbox-wrapper text-center d-flex justify-content-center" >
-                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4  d-flex justify-content-center" >
+            <div class="fbox-wrapper text-center">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
                     <!-- FEATURE BOX #1 -->
                     <div class="col">
                         <div class="fbox-6 fb-1 wow fadeInUp">
@@ -23,7 +22,7 @@
                             <div class="fbox-ico ico-55">
                                 <div class="shape-ico color--theme">
                                     <!-- Vector Icon -->
-                                    <span class="flaticon-reorder"  style="color: blue !important;"></span>
+                                    <span class="flaticon-reorder"></span>
                                     <!-- Shape -->
                                     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
@@ -33,7 +32,7 @@
                             <!-- End Icon -->
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-20 w-700">Full-on Support</h6>
+                                <h6 class="s-20 w-700">Easy Integration</h6>
                                 <p>Luctus augue egestas undo ultrice and quisque lacus</p>
                             </div>
                         </div>
@@ -46,7 +45,7 @@
                             <div class="fbox-ico ico-55">
                                 <div class="shape-ico color--theme">
                                     <!-- Vector Icon -->
-                                    <span class="flaticon-project"  style="color: blue !important;"></span>
+                                    <span class="flaticon-project"></span>
                                     <!-- Shape -->
                                     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
@@ -56,7 +55,7 @@
                             <!-- End Icon -->
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-20 w-700">Paperless Policy</h6>
+                                <h6 class="s-20 w-700">Powerful Options</h6>
                                 <p>Luctus augue egestas undo ultrice and quisque lacus</p>
                             </div>
                         </div>
@@ -69,7 +68,7 @@
                             <div class="fbox-ico ico-55">
                                 <div class="shape-ico color--theme">
                                     <!-- Vector Icon -->
-                                    <span class="flaticon-graphics"  style="color: blue !important;"></span>
+                                    <span class="flaticon-graphics"></span>
                                     <!-- Shape -->
                                     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
@@ -79,20 +78,20 @@
                             <!-- End Icon -->
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-20 w-700">Instant Policy</h6>
+                                <h6 class="s-20 w-700">Lowest Data Usage</h6>
                                 <p>Luctus augue egestas undo ultrice and quisque lacus</p>
                             </div>
                         </div>
                     </div>
                     <!-- END FEATURE BOX #3 -->
-
+                    <!-- FEATURE BOX #4 -->
                     <div class="col">
                         <div class="fbox-6 fb-4 wow fadeInUp">
                             <!-- Icon -->
                             <div class="fbox-ico ico-55">
                                 <div class="shape-ico color--theme">
                                     <!-- Vector Icon -->
-                                    <span class="flaticon-click"  style="color: blue !important;"></span>
+                                    <span class="flaticon-click"></span>
                                     <!-- Shape -->
                                     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
@@ -102,20 +101,20 @@
                             <!-- End Icon -->
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-20 w-700">Quick Commission</h6>
+                                <h6 class="s-20 w-700">Data Management</h6>
                                 <p>Luctus augue egestas undo ultrice and quisque lacus</p>
                             </div>
                         </div>
                     </div>
-
-                   
+                    <!-- END FEATURE BOX #4 -->
                 </div>
                 <!-- End row -->
             </div>
             <!-- END FEATURES-6 WRAPPER -->
         </div>
         <!-- End container -->
-     
     </section>
-    <hr class="divider divider-light" />
+    <!-- END FEATURES-6 -->
+    <!-- DIVIDER LINE -->
+    <hr class="divider" />
 </template>
