7fb91577f74ad10cb0569efd21892dbeecf7130d
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-05 17:58:41 +0530

    Update logo.vue

--- components/header/logo.vue
+++ components/header/logo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wsmobileheader clearfix">
         <!-- <NuxtLink to="/"><span class="smllogo"><img src="/assets/images/logo-pink.png" alt="mobile-logo" /></span></NuxtLink> -->
-        <NuxtLink to="/" class="logo-black"><span class="smllogo"><img src="/assets/images/gwc-logo.webp" alt="mobile-logo" style="min-height: 50px !important;" /></span></NuxtLink>
+        <NuxtLink to="/" class="logo-black"><span class="smllogo"><img src="/assets/images/gwc-logo.webp" alt="mobile-logo"  /></span></NuxtLink>
         <NuxtLink to="/" class="logo-white"><span class="smllogo"><img src="/assets/images/gwc-logo.webp" alt="logo" /></span></NuxtLink>
         <a id="wsnavtoggle" class="wsanimated-arrow" @click="toggleMobileMenu">
             <span></span>
