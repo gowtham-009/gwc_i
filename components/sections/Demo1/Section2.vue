b99e3c3e82c7a57021d5a66021cdbd7117c05f55
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-20 15:29:47 +0530

    sec2

--- components/sections/Demo1/Section2.vue
+++ components/sections/Demo1/Section2.vue
@@ -7,9 +7,9 @@
                 <div class="col-md-10 col-lg-9">
                     <div class="section-title mb-70">
                         <!-- Title -->
-                        <h2 class="s-50 w-700">3 Reason Why Becoming A POSP</h2>
+                        <h2 class="s-50 w-700">Reasons to Partner with Us                        </h2>
                         <!-- Text -->
-                        <p class="s-21 color--grey">Ligula risus auctor tempus magna feugiat lacinia.</p>
+                        <p class="s-21 color--grey">Unlock Success by Partnering with GOODWILL</p>
                     </div>
                 </div>
             </div>
@@ -25,8 +25,8 @@
                             </div>
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-22 w-700">Experience Choice with Convenience</h6>
-                                <p>Luctus egestas augue undo ultrice aliquam in lacus congue dapibus</p>
+                                <h6 class="s-22 w-700">Get Your Policy Instantly</h6>
+                                <p>Skip the long procedures and exhausting paperwork. We issue insurance policies instantly online, ensuring a hassle-free experience.</p>
                             </div>
                         </div>
                     </div>
@@ -40,8 +40,8 @@
                             </div>
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-22 w-700">Experience Dedicated <br> Support</h6>
-                                <p>Tempor laoreet augue undo ultrice aliquam in lacusq luctus feugiat</p>
+                                <h6 class="s-22 w-700">Round-the-Clock Support</h6>
+                                <p>We offer nonstop support to ensure all your queries are resolved anytime. No matter the time, our support team is here for you</p>
                             </div>
                         </div>
                     </div>
@@ -55,8 +55,8 @@
                             </div>
                             <!-- Text -->
                             <div class="fbox-txt">
-                                <h6 class="s-22 w-700">Experience Tech That's innovative Yet Simple</h6>
-                                <p>Egestas luctus augue undo ultrice aliquam in lacus feugiat cursus</p>
+                                <h6 class="s-22 w-700">Speedy Commission Clearance</h6>
+                                <p>We ensure fast commission settlements, with your account credited every 15 days after policy issuance.</p>
                             </div>
                         </div>
                     </div>
