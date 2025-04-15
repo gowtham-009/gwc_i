22169ef54f54a6de0e7462cecc617e43a2f2d78e
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-15 16:40:39 +0530

    title change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     app: {
         head: {
             meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
-            title: "Martex - Software, App, SaaS & Startup NuxtJS Template",
+            title: "Gwc india insurance",
             script: [],
             link: [
                 { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css',crossorigin:'anonymous', referrerpolicy:'no-referrer', integrity: 'sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==' },
